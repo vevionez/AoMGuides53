c0205abc0510b4a64359616ffcb32c6f0fbfb392
Eddow PC <eddorules@gmail.com>
2016-09-03 01:04:47 +0200

    I hate git (container update)

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -29,8 +29,10 @@
 
 
     <!-- Container -->
-    <div class="container">
+    <div class="container container-nav">
         @include('layouts.navbar')
+        </div>
+    <div class="container container-content">
         @if(Session::has('error'))
             <div clas="row">
             <div class="alert alert-danger">
@@ -74,6 +76,7 @@
 --}}
     </div>
         <!-- ./ container -->
+    <div clas="container container-footer">
 @include('layouts.footer')
     </div>
 @include('layouts.js_foot')
