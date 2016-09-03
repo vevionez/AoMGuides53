0ef5f5e65374d8827b80267948815516bb11c70a
Eddow PC <eddorules@gmail.com>
2016-09-03 08:30:11 +0200

    No container inside footer container pls

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,9 +1,7 @@
 <footer><div id="footer" class="footer">
-        <div class="container">
             <div class="row">
                 <p class="text-muted text-center credit">Copyright © <a href="http://aomguides.com">Aom guides</a> and design by <a
                             href="http://edwindejong.net">|TwIsTeD| Ghost</a> - <a href="/sitemap.xml">Sitemap</a></p>
 
             </div>
-        </div>
     </footer>
