27fd5bf9c8d814f6bfffd1c139caeb7b384d8c6a
Eddow PC <eddorules@gmail.com>
2016-09-03 09:11:24 +0200

    Logo placement modification

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -27,7 +27,7 @@
 <!-- Wrapper-->
 <div id="wrap">
 
-    <img class="logo-main" src="/images/AoM_Logo_trans.png" width="200" alt="Logo Thing main logo">
+    <img class="logo-main" src="/images/AoM_Logo_trans.png" width="500" alt="Age of Mythology Guides Logo">
     <!-- Container -->
     <div class="container container-nav">
         @include('layouts.navbar')
