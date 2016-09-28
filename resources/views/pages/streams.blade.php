42fa071176dedb6b78ebd78c205926a10b58be20
Eddow PC <eddorules@gmail.com>
2016-09-28 09:44:45 +0200

    getting ready for embedding- added aomvanilla

--- resources/views/pages/streams.blade.php
+++ resources/views/pages/streams.blade.php
@@ -29,6 +29,13 @@
                         url: <a href="{{ $stream['channel']['url']  }}">{{ $stream['channel']['url']  }}</a>
                     </li>
                 </ul>
+                    <button id='watch'>Watch Live!</button>";
+                    <div id='twitchtoggle' style='display: none'>
+                        <iframe id='twitchstream' frameborder='0' scrolling='no' height='378' width='620'></iframe>
+                        <div id='chat'>
+                            <iframe id='twitchchat' frameborder='0' scrolling='no' class='chat_embed' height='400' width='500'></iframe>
+                        </div>
+                    </div>
 
                 @endforeach
             @endif
@@ -51,12 +58,47 @@
                             url: <a href="{{ $stream['channel']['url']  }}">{{ $stream['channel']['url']  }}</a>
                         </li>
                     </ul>
-
+                    <button id='watch'>Watch Live!</button>";
+                    <div id='twitchtoggle' style='display: none'>
+                        <iframe id='twitchstream' frameborder='0' scrolling='no' height='378' width='620'></iframe>
+                        <div id='chat'>
+                            <iframe id='twitchchat' frameborder='0' scrolling='no' class='chat_embed' height='400' width='500'></iframe>
+                        </div>
+                    </div>
                 @endforeach
             @endif
 
             <pre>
             {{ var_dump($aomttstreams) }}
+            </pre>
+            <h2>AOM Vanilla Streams:</h2>
+            @if($aomstreams['_total'] != 0)
+                @foreach($aomstreams['streams'] as $stream)
+                    <ul>
+                        <li>
+                            Streamer: {{ $stream['channel']['display_name']  }}
+                        </li>
+                        <li>
+                            status: {{ $stream['channel']['status']  }}
+                        </li>
+                        <li>Viewers: {{ $stream['channel']['views'] }}</li>
+                        <li>Followers: {{ $stream['channel']['followers'] }}</li>
+                        <li>
+                            url: <a href="{{ $stream['channel']['url']  }}">{{ $stream['channel']['url']  }}</a>
+                        </li>
+                    </ul>
+                    <button id='watch'>Watch Live!</button>";
+                    <div id='twitchtoggle' style='display: none'>
+                        <iframe id='twitchstream' frameborder='0' scrolling='no' height='378' width='620'></iframe>
+                        <div id='chat'>
+                            <iframe id='twitchchat' frameborder='0' scrolling='no' class='chat_embed' height='400' width='500'></iframe>
+                        </div>
+                    </div>
+                @endforeach
+            @endif
+
+            <pre>
+            {{ var_dump($aomstreams) }}
             </pre>
         </div>
     </div>
