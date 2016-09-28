6c5800e60ccbbc1f307d95a1d8ca23404ba8c7ec
Eddow PC <eddorules@gmail.com>
2016-09-28 08:40:33 +0200

    need to be able to read this

--- resources/views/pages/streams.blade.php
+++ resources/views/pages/streams.blade.php
@@ -14,9 +14,13 @@
     <div class="row">
         <div class="col_lg_2">
             <h2>AOM:EE Streams:</h2>
+            <pre>
             {{ var_dump($aomeestreams) }}
+                </pre>
             <h2>AOM:TT Streams:</h2>
+            <pre>
             {{ var_dump($aomttstreams) }}
+            </pre>
         </div>
     </div>
 @stop
