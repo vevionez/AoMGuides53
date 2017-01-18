195d9fe45b46d540f9850877ac003cb00f483f16
Eddow PC <eddorules@gmail.com>
2017-01-18 05:52:59 +0100

    some more info about str

--- resources/views/pages/tools.blade.php
+++ resources/views/pages/tools.blade.php
@@ -36,10 +36,10 @@
             <h2>Useful Aom/Aot Tools:</h2>
             <ul>
                 <li>
-                    <a href="downloads/Age_Of_Mythology_Disk_1.iso">Age of Mythology Disk 1 (Direct download link)</a>
+                    <a href="downloads/Age_Of_Mythology_Disk_1.iso">Age of Mythology Disk 1 (Direct download link)</a> Serial Code: <small>qvw83-4mc67-fdpth-49pt8-6hgrd</small>
                 </li>
                 <li><a href="downloads/Age_Of_Mythology_Disk_2.iso">Age of Mythology Disk 2 (Direct download link)</a></li>
-                <li><a href="downloads/Age_Of_Mythology_The_Titans.iso">Age of Mythology: The Titans (Direct download link)</a></li>
+                <li><a href="downloads/Age_Of_Mythology_The_Titans.iso">Age of Mythology: The Titans (Direct download link)</a> Serial Code:  <small>BDKRJ-HFRHW-8W3YV-DK3MP-646VJ</small></li>
                 <li></li>
                 <li></li>
                 <li></li>
